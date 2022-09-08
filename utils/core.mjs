109c95b88ff2645a40aa82a035572f60a261dc10
久而酒知 <oncwnuKPVNaXEgz_DaQCdcY6sMZ0@git.weixin.qq.com>
2022-09-08 10:48:38 +0800

    修复文件存储问题

--- utils/core.mjs
+++ utils/core.mjs
@@ -10,49 +10,50 @@ import { convertObjToArray } from "./transform.mjs";
  * @param {String} svnPath svn路径前缀
  */
 function splitRecord(record, projectName, basePath, svnPath) {
-  const statusType = {
-    // " ": "无修改",
-    A: "增加",
-    C: "冲突",
-    D: "删除",
-    I: "忽略",
-    M: "修改",
-    R: "替换",
-    X: "未纳入版本控制的目录,被外部引用的目录所创建",
-    // "?": "未纳入版本控制",
-    "!": "该项目已遗失(被非 svn 命令删除)或不完整",
-    "~": "版本控制下的项目与其它类型的项目重名"
-  };
-  const recordMap = {};
-  const recordFileMap = {};
-  record.forEach((item) => {
-    const key = statusType[item[0]];
-    // const key = item[0];
-    const emptyKey = /\s/.test(key);
-    const index = item.indexOf(basePath);
-    const filePath = item.substring(index);
-    const path = item.substring(index).replace(basePath, svnPath);
-    if (!emptyKey && path) {
-      if (recordMap[key]) {
-        recordMap[key].push(path);
-        recordFileMap[key].push(filePath);
-      } else {
-        recordMap[key] = [path];
-        recordFileMap[key] = [filePath];
-      }
-    }
-  });
+    const statusType = {
+        // " ": "无修改",
+        A: "新增",
+        C: "冲突",
+        D: "删除",
+        I: "忽略",
+        M: "修改",
+        R: "替换",
+        X: "未纳入版本控制的目录,被外部引用的目录所创建",
+        // "?": "未纳入版本控制",
+        "!": "该项目已遗失(被非 svn 命令删除)或不完整",
+        "~": "版本控制下的项目与其它类型的项目重名"
+    };
+    const recordMap = {};
+    const recordFileMap = {};
+    record.forEach((item) => {
+        const key = statusType[item[0]];
+        // const key = item[0];
+        const emptyKey = /\s/.test(key);
+        const index = item.indexOf(basePath);
+        const filePath = item.substring(index);
+        const path = item.substring(index).replace(basePath, svnPath);
+        if (!emptyKey && path) {
+            if (recordMap[key]) {
+                recordMap[key].push(path);
+                recordFileMap[key].push(filePath);
+            } else {
+                recordMap[key] = [path];
+                recordFileMap[key] = [filePath];
+            }
+        }
+    });
 
-  // 删除不需要的文件记录
-  delete recordMap["?"];
-  delete recordMap[" "];
-  delete recordMap["undefined"];
+    // 删除不需要的文件记录
+    delete recordMap["?"];
+    delete recordMap[" "];
+    delete recordMap["undefined"];
+    delete recordFileMap["undefined"];
 
-  // 生成修改新增记录到文件中
-  const fileStr = JSON.stringify(recordMap, null, 2).replace(/\\\\/g, "/").replace(/\\r/g, "");
-  writeFile("record.json", fileStr);
-  // 复制文件到指定目录
-  copyFile(convertObjToArray(recordFileMap), "./new/", projectName);
+    // 生成修改新增记录到文件中
+    const fileStr = JSON.stringify(recordMap, null, 2).replace(/\\\\/g, "/").replace(/\\r/g, "");
+    writeFile("record.json", fileStr);
+    // 复制文件到指定目录
+    copyFile(convertObjToArray(recordFileMap), "./new/", projectName);
 }
 
 /**
@@ -62,9 +63,9 @@ function splitRecord(record, projectName, basePath, svnPath) {
  * @param {String} svnPath SVN路径前缀
  */
 export function getSvnEditPath(basePath, projectName, svnPath) {
-  const result = shell.exec(`svn status ${basePath}`, { silent: true });
-  const stdRecord = result.stdout.split("\n");
-  if (Array.isArray(stdRecord)) {
-    splitRecord(stdRecord, projectName, basePath, svnPath);
-  }
+    const result = shell.exec(`svn status ${basePath}`, { silent: true });
+    const stdRecord = result.stdout.split("\n");
+    if (Array.isArray(stdRecord)) {
+        splitRecord(stdRecord, projectName, basePath, svnPath);
+    }
 }
